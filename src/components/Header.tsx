f90c8be498a54bf92022cdc683404b07bb8650b5
Luis Gutiérrez <luisghtz@outlook.com>
2023-08-14 18:56:23 -0600

    Search ready.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,26 @@ import MyUnsplashLogo from "../assets/my_unsplash_logo.svg";
 import { AddPhoto } from "../modals/AddPhoto";
 import { ModalsContainer } from "../modals/ModalsContainer";
 
-export const Header = () => {
+type HeaderProps = {
+  onSearch: (value: string) => void;
+};
+
+export const Header = ({ onSearch }: HeaderProps) => {
   const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const [inputValue, setInputValue] = React.useState("");
+  const [searchValue, setSearchValue] = React.useState("");
+  const timeOut = React.useRef<NodeJS.Timeout>();
+
+  React.useEffect(() => {
+    clearTimeout(timeOut.current);
+    timeOut.current = setTimeout(() => {
+      setSearchValue(inputValue);
+    }, 500);
+  }, [inputValue]);
+
+  React.useEffect(() => {
+    onSearch(searchValue);
+  }, [searchValue]);
 
   return (
     <header className="header">
@@ -17,6 +35,7 @@ export const Header = () => {
             className="search__input"
             type="text"
             placeholder="Search by name"
+            onChange={e => setInputValue(e.target.value)}
           />
         </section>
       </section>

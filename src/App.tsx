a86404c09356ba5f5cf20ba008fa601179538096
Luis Gutiérrez <luisghtz@outlook.com>
2023-08-16 11:54:08 -0600

    Show photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import React from "react";
 import "./App.css";
 import { Header } from "./components/Header";
+import { useGetPhotos } from "./hooks/useGetPhotos";
+import { PhotosList } from "./components/PhotosList";
 
 function App() {
+  const { getPhotos, response, error } = useGetPhotos();
+
+  React.useEffect(() => {
+    getPhotos();
+  }, []);
+
+  const onSearch = (searchText: string) => {
+    getPhotos(searchText);
+  };
+
   return (
-    <>
-      <Header />
-    </>
+    <main className="container">
+      <Header onSearch={onSearch} />
+        {response && <PhotosList photos={response} />}
+        {response && response.length === 0 && <p className="not-found-message">No photos found</p>}
+        {error && <p className="fetch-error-message">{error}</p>}
+    </main>
   );
 }
 

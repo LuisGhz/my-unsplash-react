338d1c5c2db19a7f490daede0985f8913dfdb6af
Luis Gutiérrez <luisghtz@outlook.com>
2023-08-16 11:54:58 -0600

    Create useGetphotos.

---
+++ src/hooks/useGetPhotos.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Photo } from "../models/Photo";
+
+export const useGetPhotos = () => {
+  const [response, setResponse] = React.useState<Photo[]>();
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string>("");
+
+  const getPhotos = async (searchText: string = "") => {
+    setLoading(true);
+    let url = "http://localhost:3000/";
+
+    if (searchText !== "") {
+      url = `${url}?query=${searchText}`;
+    }
+    let response;
+    try {
+      response = await fetch(url);
+
+      if (!response.ok) throw Error();
+
+      setResponse(await response.json());
+      setLoading(false);
+    } catch {
+      setError("Error fetching data");
+      setLoading(false);
+    }
+  };
+
+  return {
+    response,
+    loading,
+    error,
+    getPhotos,
+  }
+};
